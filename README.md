bdbe3fe7a787d64ee2e155f66de87e0a1db10cb0
Jerome Swannack <jeromeswannack@gmail.com>
2025-03-09 21:05:48 +0000

    Added some insturctions to the readme

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
-# MCP Tinkering
+# MCP Transport Scratch
 
-Mainly with websockets and redis
+This is a repo containing my tinkering with websocket and HTTP based MCP transport.
+
+It contains a simple [MCP server](src/server/mcp_definition.py), that I wire up to [websockets and http](src/server/main.py). Custom transport implementations are in [transport.http](src/transport/http) and [transport.ws](src/transport/ws).
+
+I also have a [test suite](src/client/mcp_client.py) that makes some requests on the MCP server on each transport. To run the suite you need to run some stuff
+
+
+#### Setup
+
+```bash
+uv sync
+source ./.venv/bin/activate
+export PYTHONPATH=src/
+```
+
+#### Server
+
+```bash
+uvicorn server.main:app
+```
+
+#### Client
+
+```bash
+python src/client/mcp_client.py
+```
--- src/transport/ws/server.py
+++ src/transport/ws/server.py
@@ -1,5 +1,5 @@
 from mcp.server import Server
-from mcp.server.websocket import websocket_server_transport
+from mcp.server.websocket import websocket_server
 from fastapi.routing import APIRouter, WebSocket
 
 
@@ -9,7 +9,15 @@ def make_mcp_ws_server_router(mcp: Server):
     @router.websocket("/mcp/ws")
     async def websocket_endpoint(websocket: WebSocket):
         # Auth?
-        async with websocket_server_transport(websocket) as (read_stream, write_stream):
+        # TODO: improve the interface for this to just support websocket
+        async with websocket_server(
+            websocket.scope,
+            websocket._receive,
+            websocket._send,
+        ) as (
+            read_stream,
+            write_stream,
+        ):
             await mcp.run(
                 read_stream=read_stream,
                 write_stream=write_stream,
